a0a5508592422d1018e2707fa62da6c37326bd1d
Ashwin Rao <theonlyrao@gmail.com>
2016-04-15 10:46:30 -0600

    add new_manager_session_path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  namespace :manager do
+    resources :sessions, only: [:new]
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
